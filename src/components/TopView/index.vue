3788387adb8c9a07e453d9fab4c2de2927f6c62d
genu.liu <gene@genedeMacBook-Pro.local>
2022-03-01 23:28:43 +0800

    项目初始化...

---
+++ src/components/TopView/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="top-view">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬航时显示 </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬航时显示 </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬航时显示 </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬航时显示 </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopView'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
