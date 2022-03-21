d85072e5f250f17c351647720408ef08d158f18d
genu.liu <genu@cloudwise.com>
2022-03-21 23:53:42 +0800

    Code BMapView,编写utils/jsonp

---
+++ src/utils/jsonp.js
@@ -0,0 +1,23 @@
+export default function jsonp (moduleId, src, callback) {
+  return new Promise(function (resolve, reject) {
+    const dom = document.getElementById(moduleId)
+    if (dom) {
+      resolve()
+    } else {
+      const script = document.createElement('script')
+      script.src = src
+      script.id = moduleId
+      document.head.appendChild(script)
+      script.onload = function () {
+        if (Object.prototype.toString.call(callback) === '[object Function]') {
+          callback()
+        }
+        console.log('test')
+        resolve()
+      }
+      script.onerror = function () {
+        reject(new Error('异步请求失败'))
+      }
+    }
+  })
+}
---
+++ src/views/BMap.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-chart :option="options"></v-chart>
+</template>
+
+<script>
+import 'echarts/extension/bmap/bmap'
+import jsonp from '@/utils/jsonp'
+export default {
+  name: 'BMapView',
+  data () {
+    return {
+      options: {}
+    }
+  },
+  methods: {
+    initOptions () {
+      this.options = {
+        bmap: {
+          key: 'Wc62kF8LBWTNT3xOEgGOqE2diqqtPQNy',
+          center: [104.114129, 37.550339],
+          zoom: 5,
+          roam: false
+        }
+      }
+      console.log('aaa')
+    }
+  },
+  async mounted () {
+    await jsonp(
+      'bmap',
+      `http://api.map.baidu.com/getscript?v=2.0&ak=Wc62kF8LBWTNT3xOEgGOqE2diqqtPQNy&services=&t=${Date.now()}`,
+      this.initOptions.bind(this)
+    )
+  }
+}
+</script>
