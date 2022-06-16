5f1e462a1703aee8222e9a802224bf17a36b4351
genu.liu <genu@cloudwise.com>
2022-06-16 15:10:18 +0800

    add #DAT-12 添加sentry监控模块

--- src/main.js
+++ src/main.js
@@ -7,6 +7,22 @@ import VueEcharts from "vue-echarts";
 import "./style/index.css";
 import request from "./utils/request";
 import "core-js";
+import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from "@sentry/vue";
+Sentry.init({
+  Vue,
+  dsn: "https://ddbdd9c63f8f499b8ef11fc7725d4378@o1265551.ingest.sentry.io/6449812",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 Vue.config.productionTip = false;
 Vue.prototype.$echarts = echarts;
 Vue.prototype.$request = request;
