b6ef6e47e7209bd2bd77ab30e3762ca831f398df
genu.liu <genu@cloudwise.com>
2022-06-24 15:19:20 +0800

    add #DAT-12 添加aegis监控

--- wrench.config.js
+++ wrench.config.js
@@ -25,6 +25,16 @@ module.exports = {
         ])
         return config;
     },
+    aegisOptions: () => {
+        return {
+            entry: "./src/main.ts",
+            id: 'ZEn7bsvP6v1GRLXrEo', // 上报 id
+            // uin: 'xxx', // 用户唯一 ID（可选）
+            reportApiSpeed: true, // 接口测速
+            reportAssetSpeed: true, // 静态资源测速
+            spa: true // spa 应用页面跳转的时候开启 pv 计算
+        };
+    },
     build: (config) => {
         config.optimization.splitChunks.cacheGroups['commonUI'] = {
             test: /([\\/])node_modules([\\/])element-ui([\\/])/,
